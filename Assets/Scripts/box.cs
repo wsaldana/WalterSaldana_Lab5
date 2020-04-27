cea00ff0ea3d4ed820126f059a6adcd74ee01dbe
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-04-27 16:37:26 -0600

    Initial Upload

---
+++ Assets/Scripts/box.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box : MonoBehaviour
+{
+    private Color color;
+
+    void Start()
+    {
+        color = GetComponent<MeshRenderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider.gameObject.tag == "box") {
+                    hit.collider.GetComponent<Rigidbody>().AddForce(-hit.normal*3, ForceMode.Impulse);
+                }
+            }
+        }
+    }
+
+    private void OnMouseEnter() {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    private void OnMouseExit() {
+        GetComponent<MeshRenderer>().material.color = color;
+    }
+}
---
+++ Assets/Scripts/shoot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class shoot : MonoBehaviour
+{
+
+    public Slider healthBar;
+    public TextMeshProUGUI healthValue;
+    public int count;
+    public Button nextLevel;
+    public Image aim;
+
+    void Start(){
+        count = 0;
+        healthBar = GameObject.FindGameObjectWithTag("healthBar").GetComponent<Slider>();
+        healthValue = GameObject.FindGameObjectWithTag("healthCounter").GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update() {
+        Ray ray = gameObject.GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider.gameObject.tag == "lb_bird") {
+                    if (count < 3) {
+                        count += 1;
+                        updateBar();
+                    }
+                    hit.collider.gameObject.SetActive(false);
+                    //Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+
+    private void updateBar() {
+        if (healthBar) {
+            healthBar.value = count;
+            healthValue.text = count.ToString() + "/3";
+        }
+    }
+}
