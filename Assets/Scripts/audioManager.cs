db3df3afa0029dcb99a6d83dbbd1f693b2822eca
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-04-27 18:52:59 -0600

    Stuff

---
+++ Assets/Scripts/audioManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour{
+
+    public AudioClip soundTrack;
+    public AudioClip water;
+    public AudioClip shot;
+    public AudioClip reload;
+
+    private AudioSource audioSource;
+
+    void Start(){
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    
+    void Update(){
+        
+    }
+}
